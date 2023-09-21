77bce36a3aa72640969c66a8f35711ea0a87f362
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-09-21 16:18:54 -0300

    {FEAT] - Computed function on contentLayer to make TOC

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,8 +1,10 @@
 import { makeSource, defineDocumentType } from "@contentlayer/source-files";
 import readingTime from "reading-time";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
+import GithubSlugger from "github-slugger";
 
 const Blog = defineDocumentType(() => ({
   name: "Blog",
@@ -47,6 +49,28 @@ const Blog = defineDocumentType(() => ({
     readingTime: {
       type: "json",
       resolve: (doc) => readingTime(doc.body.raw),
+    },
+    toc: {
+      type: "json",
+      resolve: async (doc) => {
+        const regularExp = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+        const slugger = new GithubSlugger();
+        const headings = Array.from(
+          doc.body.raw.matchAll(regularExp).map(({ groups }) => {
+            const flag = groups?.flag;
+            const content = groups?.content;
+
+            return {
+              level:
+                flag?.length == 1 ? "one" : flag?.length == 2 ? "two" : "three",
+              text: content,
+              slug: content ? slugger.slug(content) : undefined,
+            };
+          })
+        );
+
+        return headings;
+      },
     },
   },
 }));
