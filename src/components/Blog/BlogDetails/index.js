de517da678f428c4584cbb7660c1bf0932da95e4
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-09-21 15:56:21 -0300

    [FIX] - Blog details infos and Link

--- src/components/Blog/BlogDetails/index.js
+++ src/components/Blog/BlogDetails/index.js
@@ -2,16 +2,16 @@ import { format, parseISO } from "date-fns";
 import Link from "next/link";
 import React from "react";
 
-const BlogDetails = ({ blog, slug }) => {
+const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
-    <div className="px-10 bg-accent text-light py-2 flex items-center justify-around flex-wrap font-medium mx-10 rounded-lg">
+    <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
       <time className="m-3">
         {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
       </time>
-      <span className="m-3">10 Views</span>
+      <span className="m-3">10 views</span>
       <div className="m-3">{blog.readingTime.text}</div>
-      <Link href={`/categories/${blog.slug[0]}`} className="m-3">
-        #{blog.slug[0]}
+      <Link href={`/categories/${blog.tags[0]}`} className="m-3">
+        #{blog.tags[0]}
       </Link>
     </div>
   );
