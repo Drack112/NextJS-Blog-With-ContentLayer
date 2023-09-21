fecb205ea010cd8cc47ddd5de93e5dc5ffdc7dd7
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-09-21 11:03:25 -0300

    [CHORE] - Configuration of contentLayer

--- contentlayer.confg.js
+++ contentlayer.confg.js
@@ -1,6 +1,49 @@
-import { makeSource } from "@contentlayer/source-files";
+import { makeSource, defineDocumentType } from "@contentlayer/source-files";
+
+const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: "**/**/*.mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    publishAt: {
+      type: "date",
+      required: true,
+    },
+    updatedAt: {
+      type: "date",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    image: { type: "string", required: true },
+    isPublished: {
+      type: "boolean",
+      default: false,
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
+    },
+  },
+}));
 
 export default makeSource({
   /* options */
   contentDirPath: "content",
+  documentTypes: [Blog],
 });
