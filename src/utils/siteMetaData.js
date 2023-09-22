ecfd5d663c606e1900885437f7db410c1eb82cb0
Drack112 <onepessoa3@gmail.com>
2023-09-22 11:07:03 -0300

    [FEAT] - Sitemetadata

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,19 @@
+// please update required information
+const siteMetadata = {
+  title: "Code Snippets Of Drack",
+  author: "Drack",
+  headerTitle: "Don't show this blog to my momm.",
+  description:
+    "A blog created with Next.js, Tailwind.css, contentlayer and a few more things taken from the deep web",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://code-snippets-of-drack.vercel.app/", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "onepessoa3@gmail.com",
+  github: "https://github.com/Drack112",
+  linkedin: "https://www.linkedin.com/in/4bb732229/",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
