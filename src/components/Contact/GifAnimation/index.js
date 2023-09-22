cdbcc9e5010b8c7015640f32c69fd433785c1991
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-09-22 10:33:02 -0300

    [FEAT] - NEco Arc gif render

--- src/components/Contact/GifAnimation/index.js
+++ src/components/Contact/GifAnimation/index.js
@@ -6,7 +6,13 @@ import neco from "public/neco.gif";
 const NecoArcGifAnimation = () => {
   return (
     <div>
-      <Image src={neco} alt="Neco Arg Gif" />
+      <Image
+        src={neco}
+        alt="Neco Arg Gif"
+        className="h-full object-center w-7/12 ml-20"
+        priority
+        sizes="(max-width: 1180px) 100vw, 50vw"
+      />
     </div>
   );
 };
