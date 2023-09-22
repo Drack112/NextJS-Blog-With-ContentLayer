5a4ac82a5215a0ef2ced399e126312c69325dd96
Drack112 <onepessoa3@gmail.com>
2023-09-22 13:34:59 -0300

    [FEAT] - Add dark theme switcher

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,127 @@
-import React from "react";
-
-import Logo from "@/components/Logo";
+"use client";
 import Link from "next/link";
-import { GithubIcon, LinkedinIcon, SunIcon } from "../Icons";
+import Logo from "@/components/Logo";
+import {
+  GithubIcon,
+  LinkedinIcon,
+  MoonIcon,
+  SunIcon,
+} from "@/components/Icons";
 import siteMetadata from "@/utils/siteMetaData";
+import { useState } from "react";
+import { cx } from "@/utils/cx";
+import { useThemeSwitch } from "@/components/Hooks/useThemeSwticher";
 
 const Header = () => {
+  const [mode, setMode] = useThemeSwitch();
+  const [click, setClick] = useState(false);
+
+  const toggle = () => {
+    setClick(!click);
+  };
   return (
-    <header className="w-full p-4 px-10 flex items-center justify-between">
+    <header className="w-full p-4  px-5 sm:px-10 flex items-center justify-between">
       <Logo />
-      <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
+
+      <button
+        className="inline-block sm:hidden z-50"
+        onClick={toggle}
+        aria-label="Hamburger Menu"
+      >
+        <div className="w-6 cursor-pointer transition-all ease duration-300">
+          <div className="relative">
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(-45deg) translateY(0)"
+                  : "rotate(0deg) translateY(6px)",
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                opacity: click ? 0 : 1,
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(45deg) translateY(0)"
+                  : "rotate(0deg) translateY(-6px)",
+              }}
+            >
+              &nbsp;
+            </span>
+          </div>
+        </div>
+      </button>
+
+      <nav
+        className=" w-max py-3 px-6 sm:px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center flex  sm:hidden
+        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50
+        transition-all ease duration-300
+        "
+        style={{
+          top: click ? "1rem" : "-5rem",
+        }}
+      >
         <Link href="/" className="mr-2">
           Home
         </Link>
         <Link href="/about" className="mx-2">
           About
         </Link>
-        <Link href="/contact" className="mr-2">
+        <Link href="/contact" className="mx-2">
           Contact
         </Link>
-        <button>
-          <SunIcon />
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={cx(
+            "w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1",
+            mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+          )}
+          aria-label="theme-switcher"
+        >
+          {mode === "light" ? (
+            <MoonIcon className={"fill-dark"} />
+          ) : (
+            <SunIcon className={"fill-dark"} />
+          )}
+        </button>
+      </nav>
+
+      <nav
+        className=" w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center hidden sm:flex
+        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50"
+      >
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="mx-2">
+          Contact
+        </Link>
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={cx(
+            "w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1",
+            mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+          )}
+          aria-label="theme-switcher"
+        >
+          {mode === "light" ? (
+            <MoonIcon className={"fill-dark"} />
+          ) : (
+            <SunIcon className={"fill-dark"} />
+          )}
         </button>
       </nav>
       <div className=" hidden sm:flex items-center">
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,21 +1,22 @@
-import React from "react";
-
 import Image from "next/image";
 import Link from "next/link";
-
-import profileImg from "@/public/profile-img.jpg";
+import profileImg from "public/profile-img.jpg";
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center text-dark">
-      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark mr-4">
+    <Link href="/" className="flex items-center text-dark dark:text-light">
+      <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
         <Image
           src={profileImg}
-          alt="The nerd called Drack"
+          alt="The guy called Drack logo"
           className="w-full h-auto rounded-full"
+          sizes="20vw"
+          priority
         />
       </div>
-      <span className="font-bold text-xl">The guy called Drack</span>
+      <span className="font-bold dark:font-semibold text-lg md:text-xl">
+        The guy called Drack
+      </span>
     </Link>
   );
 };
