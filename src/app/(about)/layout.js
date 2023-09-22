3cc582bf5d1f642e7105f711b6c60d8dd7b9bba8
Drack112 <onepessoa3@gmail.com>
2023-09-22 11:42:00 -0300

    [FEAT] - Make about section have insights roll

--- src/app/(about)/layout.js
+++ src/app/(about)/layout.js
@@ -1,1 +1,23 @@
-export default function AboutLayout() {}
+import InsightRoll from "@/components/InsightsRoll";
+
+const insights = [
+  "0 projects completed",
+  "2+ years of anxiety attacks",
+  "I'm a graduate of being a perfectionist with code that doesn't work.",
+  "Full of useless curiosities, but I like them.",
+  "I study programming for reasons I can't even understand.",
+  "I'm cool (my father said it)",
+  "I'm a great co-worker (I stay alone in the room)",
+  "Known professionally for being boring as f#ck.",
+  "Discord user who complains about discord.",
+  "My GitHub only has unfinished projects.",
+];
+
+export default function AboutLayout({ children }) {
+  return (
+    <main className="w-full flex flex-col items-center justify-between">
+      <InsightRoll insights={insights} />
+      {children}
+    </main>
+  );
+}
